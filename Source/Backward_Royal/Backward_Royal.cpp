cc6e5d09278aa40066ec062e262aef9d95e07fb9
Moon <wlgns2774@gmail.com>
2026-01-28 14:42:59 +0900

    Delete Source directory

--- Source/Backward_Royal/Backward_Royal.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Backward_Royal.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Backward_Royal, "Backward_Royal" );
- 
